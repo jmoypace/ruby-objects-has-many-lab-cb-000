31e8cb71c42e5be7a4c8606bc1825b526d6dc65c
jmoypace <jmoypace@gmail.com>
2018-07-17 15:05:51 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,28 @@
 class Author
 attr_accessor :name
-
+@@all=[]
 def initialize(name)
   @name=name
-
+@@all<<self
 end
-def add_post
-
+def self.all
+    @@all
+  end
+def add_post(post)
+  post.title=self
 end
-def add_post_by_title
 
+def add_post_by_title(title)
+post=Post.new(title)
+post.title=self
 end
+
 
 def posts
-  @posts
+Post.all.select {|post| title.author == self}
 end
+
+def self.post_count
+    Post.all.count
+  end
 end
