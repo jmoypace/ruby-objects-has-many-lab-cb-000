a41754b24d12878df6e74d44168bc17944dbe910
jmoypace <jmoypace@gmail.com>
2018-07-17 14:43:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,17 +5,25 @@ def initialize(name)
   @name=name
   @@all<<self
 end
+
+def self.all
+    @@all
+  end
+
 def add_song(song)
-  @songs<<song
   song.artist=self
 end
-def add_song_by_name(name, genre)
-song=Song.new(name, genre)
-@songs=song
+
+def add_song_by_name(name)
+song=Song.new(name)
 song.artist=self
 end
+
 def songs
-  @songs
+  Song.all.select {|song| song.artist == self}
 end
 
+def self.song_count
+    Song.all.count
+  end
 end
