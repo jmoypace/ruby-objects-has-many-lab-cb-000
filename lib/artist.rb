4149a82a1068c43f84b6002145d874d0420aa6ba
jmoypace <jmoypace@gmail.com>
2018-07-17 14:30:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,8 +9,10 @@ def add_song(song)
   @songs<<song
   song.artist=self
 end
-def add_song_by_name
-
+def add_song_by_name(name, genre)
+song=Song.new(name, genre)
+@songs=song
+song.artist=self
 end
 
 end
